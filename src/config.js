c7f5287362a1f76007eebbcefbd3ed6254b61499
Vincent Moutel <vmoutel@yahoo.fr>
2024-03-18 15:59:26 +0000

    updated with config.js

---
+++ src/config.js
@@ -0,0 +1,12 @@
+
+
+// API
+export const REACT_APP_API_SERVER_URL='http://127.0.0.1:5000/api/v0'
+export const REACT_APP_API_AUDIENCE='https://quid3_api.com'
+
+// Auth0
+export const REACT_APP_AUTH0_DOMAIN='quid3.eu.auth0.com'
+export const REACT_APP_AUTH0_CLIENT_ID='sbBjTrKO3hGLfnsMEAE9KLKlbhTg44eg'
+export const REACT_APP_AUTH0_SCOPE="get:qandas post:qandas"
+
+
--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { REACT_APP_API_AUDIENCE, REACT_APP_AUTH0_SCOPE, REACT_APP_AUTH0_DOMAIN, REACT_APP_AUTH0_CLIENT_ID } from "./config"
+
+// const auth_domain = process.env.REACT_APP_AUTH0_DOMAIN;
+// const auth_clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+// const auth_scope = process.env.REACT_APP_AUTH0_SCOPE;
+// const api_audience = process.env.REACT_APP_API_AUDIENCE;
 
 
-const auth_domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const auth_clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-const auth_scope = process.env.REACT_APP_AUTH0_SCOPE;
-const api_audience = process.env.REACT_APP_API_AUDIENCE;
+const auth_domain = REACT_APP_AUTH0_DOMAIN;
+const auth_clientId = REACT_APP_AUTH0_CLIENT_ID;
+const auth_scope = REACT_APP_AUTH0_SCOPE;
+const api_audience = REACT_APP_API_AUDIENCE;
 
 console.log("auth_domain", auth_domain)
 console.log("auth_clientId", auth_clientId)
