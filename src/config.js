18b4d221d1f975d6ef7d6c5787853ad20e0a76b9
Vincent Moutel <vmoutel@yahoo.fr>
2024-03-19 17:35:45 +0000

    updated api url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 
 
 // API
-export const REACT_APP_API_SERVER_URL='http://18.134.206.88/api/v0'
+export const REACT_APP_API_SERVER_URL='http://turning.rocks/api/v0'
+// export const REACT_APP_API_SERVER_URL='http://18.134.206.88/api/v0'
 // export const REACT_APP_API_SERVER_URL='http://127.0.0.1:5000/api/v0'
 export const REACT_APP_API_AUDIENCE='https://quid3_api.com'
 
